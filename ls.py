131e3bd7c2d3bd8167f064c9f7c926e9bf352cd0
Rob Lanphier <robla@robla.net>
2017-08-02 21:27:04 -0700

    ls.py: Better version using scandir from Python 3.5

--- ls.py
+++ ls.py
@@ -8,40 +8,29 @@ def parse_arguments():
     """ see http://docs.python.org/library/argparse """
     parser = argparse.ArgumentParser(
         description='Get a file listing in a more scriptable way')
-    parser.add_argument('--url', dest='url', type=str, help='a url that might be interesting')
-    parser.add_argument('files', help='some files, of which there may not be any', 
-        nargs='*', default=None)
-    parser.add_argument('-b', '--bmybaby',
-                    help='be my...be my baby',
-                    action="store_true")
+    parser.add_argument('files', help='some files, of which there may not be any',
+                        nargs='*', default=['.'])
     return parser.parse_args()
 
 
-def ls():
+def ls(files=['.']):
+    """
+    Get file listing of pwd in a more scriptable way using scandir from
+    Python 3.5
+    """
     import os
     import sys
-    print("== args.url ==")
-    for subdir, dirs, files in os.walk("."):
-        for f in dirs + files:
-            ino = os.lstat(os.path.join(subdir, f)).st_ino
-            sys.stdout.write("%d %s %s\n" % (ino, subdir, f))
+    for f in files:
+        for entry in os.scandir(f):
+            print(entry.name)
 
 
 def main(argv=None):
     """ A program that uses argparse """
-    ls()
     args = parse_arguments()
-    print("== args.url ==")
-    print(args.url)
+    ls(args.files)
 
-    print("== args.files ==")
-    print(args.files)
-
-    print("== args.bmybaby ==")
-    print(args.bmybaby)
 
 if __name__ == '__main__':
     exit_status = main(sys.argv)
     sys.exit(exit_status)
-
-
