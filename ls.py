3cf3836a65cf518a7f1c3a3b995a27a21a4a2011
Rob Lanphier <robla@robla.net>
2017-08-02 20:58:19 -0700

    ls.py: Adding the stackexchange answer

--- ls.py
+++ ls.py
@@ -7,7 +7,7 @@ import sys
 def parse_arguments():
     """ see http://docs.python.org/library/argparse """
     parser = argparse.ArgumentParser(
-        description='A program that uses argparse')
+        description='Get a file listing in a more scriptable way')
     parser.add_argument('--url', dest='url', type=str, help='a url that might be interesting')
     parser.add_argument('files', help='some files, of which there may not be any', 
         nargs='*', default=None)
@@ -17,9 +17,19 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def ls():
+    import os
+    import sys
+    print("== args.url ==")
+    for subdir, dirs, files in os.walk("."):
+        for f in dirs + files:
+            ino = os.lstat(os.path.join(subdir, f)).st_ino
+            sys.stdout.write("%d %s %s\n" % (ino, subdir, f))
+
+
 def main(argv=None):
     """ A program that uses argparse """
-
+    ls()
     args = parse_arguments()
     print("== args.url ==")
     print(args.url)
