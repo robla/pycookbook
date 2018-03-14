22f921cc5acae29db7a1247e574a99fc302f1ef8
Rob Lanphier <robla@robla.net>
2018-03-14 15:45:00 -0700

    Getting rid of argparse-dsparse-example.py; didn't meet expectations
    I'm actually making this commit on 2018-06-08, and reconstructing my
    thinking from back then.  As I recall, dsparse had a weird flaw or two,
    and wasn't worth the pain of using something that wasn't installed as
    widely.  So going back to my simpler version with just argparse.

--- argparse-example.py
+++ argparse-example.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
+"""
+A program that uses argparse.
+
+Change the docstring above.  Srsly
+"""
 
 import argparse
 import sys
 
 
-def parse_arguments():
-    """ see http://docs.python.org/library/argparse """
-    parser = argparse.ArgumentParser(
-        description='A program that uses argparse')
-    parser.add_argument('--url', dest='url', type=str,
-                        help='a url that might be interesting')
-    parser.add_argument('files', help='some files, of which there may not be any',
-                        nargs='*', default=None)
-    parser.add_argument('-b', '--bmybaby',
-                        help='be my...be my baby',
-                        action="store_true")
-    return parser.parse_args()
+def printallargs(args):
+    """
+    Print all of the arguments
+    """
 
-
-def main(argv=None):
-    """ A program that uses argparse """
-
-    args = parse_arguments()
     print("== args.url ==")
     print(args.url)
 
@@ -31,8 +23,23 @@ def main(argv=None):
     print("== args.bmybaby ==")
     print(args.bmybaby)
 
+
+def main(argv=None):
+    # using splitlines to just get the first line
+    parser = argparse.ArgumentParser(description=__doc__.splitlines()[1])
+
+    parser.add_argument('--url', dest='url', type=str,
+                        help='a url that might be interesting')
+    parser.add_argument('files', help='some files, of which there may not be any',
+                        nargs='*', default=None)
+    parser.add_argument('-b', '--bmybaby',
+                        help='be my...be my baby',
+                        action="store_true")
+    args = parser.parse_args()
+
+    printallargs(args)
+
+
 if __name__ == '__main__':
     exit_status = main(sys.argv)
     sys.exit(exit_status)
-
-
