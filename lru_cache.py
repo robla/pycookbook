e4a70368c0fc89a619e63af005fbf9ce8231fb10
Rob Lanphier <robla@robla.net>
2018-06-18 22:54:07 -0700

    Added lru_cache.py

---
+++ lru_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from functools import lru_cache
+import urllib
+import urllib.error
+import urllib.request
+
+# Copied from https://docs.python.org/3.5/library/functools.html
+
+@lru_cache(maxsize=32)
+def get_pep(num):
+    'Retrieve text of a Python Enhancement Proposal'
+    resource = 'http://www.python.org/dev/peps/pep-%04d/' % num
+    try:
+        with urllib.request.urlopen(resource) as s:
+            return s.read()
+    except urllib.error.HTTPError:
+        return 'Not Found'
+
+for n in 8, 290, 308, 320, 8, 218, 320, 279, 289, 320, 9991:
+    pep = get_pep(n)
+    print(n, len(pep))
+
+print(get_pep.cache_info())
