379bf74e5c29960d98430ba2c4d65c044937ee11
Rob Lanphier <robla@robla.net>
2020-10-13 22:14:56 -0700

    Adding a couple of prompt-toolkit scripts from last year.

---
+++ prompt-toolkit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from __future__ import unicode_literals
+from prompt_toolkit import prompt
+
+def main():
+    text = prompt('> ')
+    print('You entered:', text)
+
+if __name__ == '__main__':
+    main()
